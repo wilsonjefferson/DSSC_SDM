d0e054d52bcbc7258eb8f402aa759aee3a61f28f
wilsonjefferson <pierpa.m.96@live.it>
2021-04-01 09:52:27 +0200

    added quentin project and presentation, previous files are deleted

--- progetto_prova/src/Test_02.java
+++
@@ -1,4 +0,0 @@
-public class Test_02 extends  Test_01{
-    int y;
-
-}
--- progetto_prova/src/Vehicle_Interface.java
+++
@@ -1,8 +0,0 @@
-public interface Vehicle_Interface {
-    public String getTarga();
-    public int getKilometraggio();
-    public void setNposti(int posti);
-    public static void speak(){ // static method has a body in Interfaces
-        System.out.println("Hello human, I'm a Transformer!");
-    }
-}
